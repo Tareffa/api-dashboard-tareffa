a17a002abd54e0f737823ba77b9815568d7bde92
CrispimPDs <crispimpds5@gmail.com>
2019-07-23 13:42:09 -0300

    Teste Filtro Departamento

--- src/main/java/br/com/ottimizza/dashboard/repositories/servicoProgramado/ServicoProgramadoRepositoryImpl.java
+++ src/main/java/br/com/ottimizza/dashboard/repositories/servicoProgramado/ServicoProgramadoRepositoryImpl.java
@@ -1,8 +1,13 @@
 package br.com.ottimizza.dashboard.repositories.servicoProgramado;
 
+import br.com.ottimizza.dashboard.models.departamentos.QDepartamento;
+import br.com.ottimizza.dashboard.models.empresas.QEmpresa;
+import br.com.ottimizza.dashboard.models.empresas.QEmpresaShort;
+import br.com.ottimizza.dashboard.models.servicos.QServico;
 import br.com.ottimizza.dashboard.models.servicos.QServicoProgramado;
 import br.com.ottimizza.dashboard.models.servicos.ServicoProgramado;
 import br.com.ottimizza.dashboard.models.servicos.ServicoProgramadoFiltroAvancado;
+import br.com.ottimizza.dashboard.models.usuarios.QUsuario;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import javax.persistence.EntityManager;
@@ -16,15 +21,32 @@ public class ServicoProgramadoRepositoryImpl implements ServicoProgramadoReposit
     EntityManager em;
     
     private QServicoProgramado servicoProgramado = QServicoProgramado.servicoProgramado;
+    private QUsuario usuario = QUsuario.usuario;
+    private QDepartamento departamento = QDepartamento.departamento;
+    private QEmpresaShort empresa = QEmpresaShort.empresaShort;
+    private QServico servico = QServico.servico;
 
     @Override
     public Long contadorServicoProgramado(ServicoProgramadoFiltroAvancado filtro) {
         
         JPAQuery query = new JPAQuery(em);
-        query.from(servicoProgramado);
+        query.from(servicoProgramado)
+            .innerJoin(servico).on(servicoProgramado.servico.id.eq(servico.id))
+            .innerJoin(empresa).on(servicoProgramado.cliente.id.eq(empresa.id))
+            .innerJoin(usuario).on(servicoProgramado.alocadoPara.id.eq(usuario.id))
+            .innerJoin(departamento).on(usuario.departamento.id.eq(departamento.id));
         
-        //STATUS SERVIÇO PROGRAMADO
-        if(filtro.getStatus() != null) query.where(servicoProgramado.status.in(filtro.getStatus()));
+        //*** FILTRO SERVIÇOS PROGRAMADOS ***//
+        //STATUS
+        if(filtro.getStatus() != null)query.where(servicoProgramado.status.in(filtro.getStatus()));
+        
+        //DEPARTAMENTO
+        if(filtro.getDepartamento() != null && filtro.getDepartamento().getId() != null) 
+        query.where(departamento.id.in(filtro.getDepartamento().getId()));
+        
+        //USUARIO
+//        if(filtro.getUsuario() != null && filtro.getUsuario().getId() != null) 
+//        query.where(servicoProgramado.status.in(filtro.getStatus()));
         
         return query.fetchCount();
     }
