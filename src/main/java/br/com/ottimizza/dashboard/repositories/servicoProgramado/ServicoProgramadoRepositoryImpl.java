7d56e50545ba2aac6322dbb4d1483d49236ae916
CrispimPDs <crispimpds5@gmail.com>
2019-07-25 08:46:52 -0300

    Ajuste serviço programado controller

--- src/main/java/br/com/ottimizza/dashboard/repositories/servicoProgramado/ServicoProgramadoRepositoryImpl.java
+++ src/main/java/br/com/ottimizza/dashboard/repositories/servicoProgramado/ServicoProgramadoRepositoryImpl.java
@@ -30,76 +30,83 @@ public class ServicoProgramadoRepositoryImpl implements ServicoProgramadoReposit
 
     @Override
     public Long contadorServicoProgramado(ServicoProgramadoFiltroAvancado filtro) {
+
+        System.out.println("INICIANDO CONTAGEM");
+        try {    
         
         JPAQuery query = new JPAQuery(em);
-        query.from(servicoProgramado)
-            .innerJoin(servico).on(servicoProgramado.servico.id.eq(servico.id)) //JOIN SERVIÇO
-            .innerJoin(empresa).on(servicoProgramado.cliente.id.eq(empresa.id)) //JOIN EMPRESA
-            .innerJoin(usuario).on(servicoProgramado.alocadoPara.id.eq(usuario.id)) //JOIN USUÁRIO
-            .innerJoin(departamento).on(usuario.departamento.id.eq(departamento.id)); //JOIN DEPARTAMENTO
-        
-        /*** FILTRO SERVIÇOS PROGRAMADOS ***/
-        
-            //STATUS
-            if(filtro.getSituacao() != null){
-                
-                //ABERTO
-                if(filtro.getSituacao() == ServicoProgramadoSituacao.ABERTO) {
-                    query.where(servicoProgramado.status.in(ServicoProgramadoStatus.NAO_INICIADO,ServicoProgramadoStatus.INICIADO));
-                
-                    if(filtro.getPrazo() != null){
-                        Date dataAtual = new Date();
-                        if(filtro.getPrazo() == ServicoProgramadoPrazo.NO_PRAZO)
-                            query.where(servicoProgramado.dataProgramadaEntrega.goe(dataAtual));
-                        
-                        if(filtro.getPrazo() == ServicoProgramadoPrazo.ATRASADO){
-                            query.where(servicoProgramado.dataProgramadaEntrega.lt(dataAtual));
-                            query.where(servicoProgramado.dataVencimento.goe(dataAtual));
+            query.from(servicoProgramado)
+                .innerJoin(servico).on(servicoProgramado.servico.id.eq(servico.id)) //JOIN SERVIÇO
+                .innerJoin(empresa).on(servicoProgramado.cliente.id.eq(empresa.id)) //JOIN EMPRESA
+                .innerJoin(usuario).on(servicoProgramado.alocadoPara.id.eq(usuario.id)) //JOIN USUÁRIO
+                .innerJoin(departamento).on(usuario.departamento.id.eq(departamento.id)); //JOIN DEPARTAMENTO
+
+            /*** FILTRO SERVIÇOS PROGRAMADOS ***/
+
+                //STATUS
+                if(filtro.getSituacao() != null){
+
+                    //ABERTO
+                    if(filtro.getSituacao() == ServicoProgramadoSituacao.ABERTO) {
+                        query.where(servicoProgramado.status.in(ServicoProgramadoStatus.NAO_INICIADO,ServicoProgramadoStatus.INICIADO));
+
+                        if(filtro.getPrazo() != null){
+                            Date dataAtual = new Date();
+                            if(filtro.getPrazo() == ServicoProgramadoPrazo.NO_PRAZO)
+                                query.where(servicoProgramado.dataProgramadaEntrega.goe(dataAtual));
+
+                            if(filtro.getPrazo() == ServicoProgramadoPrazo.ATRASADO){
+                                query.where(servicoProgramado.dataProgramadaEntrega.lt(dataAtual));
+                                query.where(servicoProgramado.dataVencimento.goe(dataAtual));
+                            }
+
+                            if(filtro.getPrazo() == ServicoProgramadoPrazo.VENCIDO)
+                                query.where(servicoProgramado.dataVencimento.lt(dataAtual));
                         }
-                        
-                        if(filtro.getPrazo() == ServicoProgramadoPrazo.VENCIDO)
-                            query.where(servicoProgramado.dataVencimento.lt(dataAtual));
+                    }
+
+                    //ENCERRADO
+                    if(filtro.getSituacao() == ServicoProgramadoSituacao.ENCERRADO){
+                        query.where(servicoProgramado.status.in(ServicoProgramadoStatus.CONCLUIDO,ServicoProgramadoStatus.ENVIADO));
+
+                        if(filtro.getPrazo() != null){
+                            if(filtro.getPrazo() == ServicoProgramadoPrazo.NO_PRAZO)
+                                query.where(servicoProgramado.dataProgramadaEntrega.goe(servicoProgramado.dataTermino));
+
+                            if(filtro.getPrazo() == ServicoProgramadoPrazo.ATRASADO){
+                                query.where(servicoProgramado.dataProgramadaEntrega.lt(servicoProgramado.dataTermino));
+                                query.where(servicoProgramado.dataVencimento.goe(servicoProgramado.dataTermino));
+                            }
+
+                            if(filtro.getPrazo() == ServicoProgramadoPrazo.VENCIDO)
+                                query.where(servicoProgramado.dataVencimento.lt(servicoProgramado.dataTermino));
+                        } 
                     }
                 }
-                
-                //ENCERRADO
-                if(filtro.getSituacao() == ServicoProgramadoSituacao.ENCERRADO){
-                    query.where(servicoProgramado.status.in(ServicoProgramadoStatus.CONCLUIDO,ServicoProgramadoStatus.ENVIADO));
-                 
-                    if(filtro.getPrazo() != null){
-                        if(filtro.getPrazo() == ServicoProgramadoPrazo.NO_PRAZO)
-                            query.where(servicoProgramado.dataProgramadaEntrega.goe(servicoProgramado.dataTermino));
-                        
-                        if(filtro.getPrazo() == ServicoProgramadoPrazo.ATRASADO){
-                            query.where(servicoProgramado.dataProgramadaEntrega.lt(servicoProgramado.dataTermino));
-                            query.where(servicoProgramado.dataVencimento.goe(servicoProgramado.dataTermino));
-                        }
-                        
-                        if(filtro.getPrazo() == ServicoProgramadoPrazo.VENCIDO)
-                            query.where(servicoProgramado.dataVencimento.lt(servicoProgramado.dataTermino));
-                    } 
-                }
-            }
-            
-            //DEPARTAMENTO
-            if(filtro.getDepartamento() != null && filtro.getDepartamento().getId() != null) 
-            query.where(departamento.id.in(filtro.getDepartamento().getId()));
 
-            //USUARIO
-            if(filtro.getUsuario() != null && filtro.getUsuario().getId() != null) 
-            query.where(usuario.id.in(filtro.getUsuario().getId()));
+                //DEPARTAMENTO
+                if(filtro.getDepartamento() != null && filtro.getDepartamento().getId() != null) 
+                query.where(departamento.id.in(filtro.getDepartamento().getId()));
 
-            //SERVIÇO
-            if(filtro.getServico() != null && filtro.getServico().getId() != null) 
-            query.where(servico.id.in(filtro.getServico().getId()));
+                //USUARIO
+                if(filtro.getUsuario() != null && filtro.getUsuario().getId() != null) 
+                query.where(usuario.id.in(filtro.getUsuario().getId()));
 
-            //EMPRESA
-            if(filtro.getEmpresa() != null && filtro.getEmpresa().getId() != null) 
-            query.where(empresa.id.in(filtro.getEmpresa().getId()));
+                //SERVIÇO
+                if(filtro.getServico() != null && filtro.getServico().getId() != null) 
+                query.where(servico.id.in(filtro.getServico().getId()));
 
-        /*** FIM FILTRO SERVIÇOS PROGRAMADOS ***/
-        
-        return query.fetchCount();
+                //EMPRESA
+                if(filtro.getEmpresa() != null && filtro.getEmpresa().getId() != null) 
+                query.where(empresa.id.in(filtro.getEmpresa().getId()));
+
+            /*** FIM FILTRO SERVIÇOS PROGRAMADOS ***/
+
+            return query.fetchCount();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new Long(-1);
+        }
     }
     
 }
