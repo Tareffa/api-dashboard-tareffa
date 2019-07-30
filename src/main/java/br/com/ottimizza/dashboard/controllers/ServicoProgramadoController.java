c8b5f8bbd835796e321ea7683bc69034ecf63139
CrispimPDs <crispimpds5@gmail.com>
2019-07-30 12:01:29 -0300

    Teste agrupamento

--- src/main/java/br/com/ottimizza/dashboard/controllers/ServicoProgramadoController.java
+++ src/main/java/br/com/ottimizza/dashboard/controllers/ServicoProgramadoController.java
@@ -4,10 +4,12 @@ import br.com.ottimizza.dashboard.models.servicos.ServicoProgramadoFiltroAvancad
 import br.com.ottimizza.dashboard.services.ServicoProgramadoService;
 import br.com.ottimizza.dashboard.services.UserService;
 import java.security.Principal;
+import java.util.LinkedHashMap;
 import javax.inject.Inject;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,11 +29,13 @@ public class ServicoProgramadoController {
 
     @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     // <editor-fold defaultstate="collapsed" desc="Find company by ID">
-    public ResponseEntity<String> findCompanyByID(Principal principal, @RequestBody ServicoProgramadoFiltroAvancado filtro)
+    public ResponseEntity<String> countServiceProgram(OAuth2Authentication authentication, @RequestBody ServicoProgramadoFiltroAvancado filtro)
             throws Exception {
-
+        LinkedHashMap<String, Object> properties = (LinkedHashMap<String, Object>) authentication.getUserAuthentication().getDetails();
         // Get Authorized User by Username.
-        System.out.println("Principal: " + principal.getName());
+        System.out.println("TUDAO: " + properties.toString());
+        System.out.println("Email: " + properties.get("email"));
+        
         //User authorized = userService.findByUsername(principal.getName());
         //ServicoProgramadoFiltroAvancado filtro = new ServicoProgramadoFiltroAvancado();
 
