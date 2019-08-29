49ce122330bc106958999e3a41745f3c29916aca
CrispimPDs <crispimpds5@gmail.com>
2019-08-29 11:07:34 -0300

    Teste Oauth

--- src/main/java/br/com/ottimizza/dashboard/controllers/OAuthClientController.java
+++ src/main/java/br/com/ottimizza/dashboard/controllers/OAuthClientController.java
@@ -5,14 +5,12 @@ import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.io.IOException;
-import java.net.URI;
 import java.text.MessageFormat;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -61,18 +59,17 @@ public class OAuthClientController {
     @PostMapping(value = "/refresh", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<String> oauthRefresh(@RequestParam("refresh_token") String refreshToken,
             @RequestParam("client_id") String clientId) throws IOException {
-        
         String credentials = OAUTH2_CLIENT_ID + ":" + OAUTH2_CLIENT_SECRET;
         String encodedCredentials = Base64.getEncoder().encodeToString(credentials.getBytes());
 
         try {
             HttpClient httpClient = HttpClientBuilder.create().build();
 
-            URIBuilder uriBuilder = new URIBuilder(OAUTH2_SERVER_URL + "/oauth/token");
-            uriBuilder.addParameter("refresh_token", refreshToken);
-            uriBuilder.addParameter("grant_type", "refresh_token");
+            String uri = MessageFormat.format("{0}/oauth/token?grant_type={1}&refresh_token={2}", 
+                    OAUTH2_SERVER_URL, "refresh_token", refreshToken
+            );
 
-            HttpPost httpPost = new HttpPost(uriBuilder.build());
+            HttpPost httpPost = new HttpPost(uri);
 
             httpPost.setHeader("Authorization", "Basic " + encodedCredentials);
 
