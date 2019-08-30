7002a2a93f8ab6de45b1522015b6f901efd6df77
CrispimPDs <crispimpds5@gmail.com>
2019-08-30 08:43:32 -0300

    Ajuste de requisição - serviço programado

--- src/main/java/br/com/ottimizza/dashboard/services/ServicoProgramadoService.java
+++ src/main/java/br/com/ottimizza/dashboard/services/ServicoProgramadoService.java
@@ -1,13 +1,15 @@
 package br.com.ottimizza.dashboard.services;
 
+import br.com.ottimizza.dashboard.constraints.ServicoProgramadoPrazo;
+import br.com.ottimizza.dashboard.constraints.ServicoProgramadoSituacao;
 import br.com.ottimizza.dashboard.models.servicos.QServico;
 import br.com.ottimizza.dashboard.models.servicos.QServicoProgramado;
 import br.com.ottimizza.dashboard.models.servicos.ServicoProgramadoFiltroAvancado;
 import br.com.ottimizza.dashboard.models.servicos.ServicoProgramadoFiltroAvancadoDataProgramado;
 import br.com.ottimizza.dashboard.models.usuarios.Usuario;
 import br.com.ottimizza.dashboard.repositories.servicoProgramado.ServicoProgramadoRepository;
-import java.util.Calendar;
-import java.util.Date;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import javax.inject.Inject;
 import org.json.JSONObject;
@@ -25,7 +27,22 @@ public class ServicoProgramadoService {
     //<editor-fold defaultstate="collapsed" desc="Count scheduled service">
     public JSONObject count(ServicoProgramadoFiltroAvancado filtro, Usuario autenticado)throws Exception{
         JSONObject resultado = new JSONObject();
-        resultado.put("tamanho", repository.contadorServicoProgramado(filtro, autenticado));
+        
+        //ABERTO
+        filtro.setSituacao(ServicoProgramadoSituacao.ABERTO);
+        filtro.setPrazo(Arrays.asList(ServicoProgramadoPrazo.NO_PRAZO));
+        resultado.put("abertoNoPrazo", repository.contadorServicoProgramado(filtro, autenticado));
+        
+        filtro.setPrazo(Arrays.asList(ServicoProgramadoPrazo.ATRASADO,ServicoProgramadoPrazo.VENCIDO));
+        resultado.put("abertoAtrasado", repository.contadorServicoProgramado(filtro, autenticado));
+        
+        //ENCERRADO
+        filtro.setSituacao(ServicoProgramadoSituacao.ENCERRADO);
+        filtro.setPrazo(Arrays.asList(ServicoProgramadoPrazo.NO_PRAZO));
+        resultado.put("encerradoNoPrazo", repository.contadorServicoProgramado(filtro, autenticado));
+        filtro.setPrazo(Arrays.asList(ServicoProgramadoPrazo.ATRASADO,ServicoProgramadoPrazo.VENCIDO));
+        resultado.put("encerradoAtrasado", repository.contadorServicoProgramado(filtro, autenticado));
+        
         return resultado;
     }
     //</editor-fold>
