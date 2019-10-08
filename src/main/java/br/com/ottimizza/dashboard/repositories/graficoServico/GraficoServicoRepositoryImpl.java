6f868c93b5228c15224801bdea42935c714eae7f
CrispimPDs <crispimpds5@gmail.com>
2019-10-08 11:09:48 -0300

    Teste de query

--- src/main/java/br/com/ottimizza/dashboard/repositories/graficoServico/GraficoServicoRepositoryImpl.java
+++ src/main/java/br/com/ottimizza/dashboard/repositories/graficoServico/GraficoServicoRepositoryImpl.java
@@ -102,19 +102,12 @@ public class GraficoServicoRepositoryImpl implements GraficoServicoRepositoryCus
             
             JPAQuery<ServicoShort> query = new JPAQuery(em);
             query.from(servico)
-//                .where(
-//                    Expressions.booleanTemplate(
-//                        " NOT EXISTS {0} ", 
-//                        JPAExpressions.select(graficoServico)
-//                            .where(graficoServico.servico.id.eq(servico.id))
-//                            .where(graficoServico.grafico.id.eq(graficoId))
-//                    )
-//                )
-                .where(servico.contabilidade.id.eq(usuario.getContabilidade().getId()));
+                .innerJoin(graficoServico).on(
+                    graficoServico.servico.id.eq(servico.id)
+                    .and(graficoServico.grafico.id.eq(graficoId)))
+                .where(servico.contabilidade.id.eq(usuario.getContabilidade().getId()))
+                .where(graficoServico.grafico.id.isNull());
             System.out.println("QUERY COMPLETO: " + query.toString());
-//                .innerJoin(graficoServico).on(graficoServico.servico.id.eq(servico.id))
-//                .where(servico.contabilidade.id.eq(usuario.getContabilidade().getId()))
-//                .where(graficoServico.grafico.id.isNull());
 
             query.select(Projections.constructor(ServicoShort.class, servico.id, servico.nome, servico.contabilidade, servico.permiteBaixaManual));
 
