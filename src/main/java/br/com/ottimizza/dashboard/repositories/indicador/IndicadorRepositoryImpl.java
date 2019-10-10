501ac69cb19a5454623902250cab01d789bf138b
CrispimPDs <crispimpds5@gmail.com>
2019-10-10 16:28:48 -0300

    Teste de exclusão indicador query

--- src/main/java/br/com/ottimizza/dashboard/repositories/indicador/IndicadorRepositoryImpl.java
+++ src/main/java/br/com/ottimizza/dashboard/repositories/indicador/IndicadorRepositoryImpl.java
@@ -1,5 +1,6 @@
 package br.com.ottimizza.dashboard.repositories.indicador;
 
+import br.com.ottimizza.dashboard.models.graficos.QGrafico;
 import br.com.ottimizza.dashboard.models.indicadores.Indicador;
 import br.com.ottimizza.dashboard.models.indicadores.IndicadorShort;
 import br.com.ottimizza.dashboard.models.indicadores.QIndicador;
@@ -20,6 +21,7 @@ public class IndicadorRepositoryImpl implements IndicadorRepositoryCustom{
     EntityManager em;
     
     private QIndicador indicador = QIndicador.indicador;
+    private QGrafico grafico = QGrafico.grafico;
     
     @Override
     public Indicador buscarIndicadorPorId(BigInteger indicadorId, Usuario usuario) {
@@ -65,10 +67,22 @@ public class IndicadorRepositoryImpl implements IndicadorRepositoryCustom{
     @Override
     public boolean excluirIndicadorPorId(BigInteger indicadorId, Usuario usuario) throws Exception {
         try {
-            new JPADeleteClause(em,indicador)
-                .where(indicador.contabilidade.id.eq(usuario.getContabilidade().getId())) //CONTABILIDADE
-                .where(indicador.id.eq(indicadorId) //INDICADOR ID
-            ).execute();
+            //GRAFICO_CARACTERISTICA (GRAFICO_ID)
+            //GRAFICO_SERVICO (GRAFICO_ID)
+            //GRAFICO (INDICADOR_ID)
+            System.out.println("QUERY DELETE:" + 
+                new JPADeleteClause(em,grafico)
+                    .where(grafico.indicador.contabilidade.id.eq(usuario.getContabilidade().getId())) //CONTABILIDADE
+                    .where(grafico.indicador.id.eq(indicadorId) //INDICADOR ID
+                ).toString()
+            );
+            //.execute();            
+            
+            //INDICADOR
+//            new JPADeleteClause(em,indicador)
+//                .where(indicador.contabilidade.id.eq(usuario.getContabilidade().getId())) //CONTABILIDADE
+//                .where(indicador.id.eq(indicadorId) //INDICADOR ID
+//            ).execute();
 
             return true;
         } catch (Exception e) {
